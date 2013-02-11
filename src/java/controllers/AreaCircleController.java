0499f726908dd6d9d88ea1de593747291e563a87
Jessie <Jessie@10.30.16.107>
2013-02-11 13:36:03 -0600

    hope to get this right!!

--- src/java/controllers/AreaCircleController.java
+++ src/java/controllers/AreaCircleController.java
@@ -35,7 +35,7 @@ public class AreaCircleController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
             AreaCircle ac = new AreaCircle();
         double areaCircle = ac.getAreaOfCircle();
-        request.getParameter("areaCircle");
+        //request.getParameter("areaCircle");
         request.setAttribute("areaCircle", areaCircle);
        
         RequestDispatcher view =
